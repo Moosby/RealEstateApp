4b0bc0f54856baa1d594e20fd8586009e7d7b879
Moosby <125193616+Moosby@users.noreply.github.com>
2026-08-08 20:37:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'real-estate-offline-v13';
+const CACHE_NAME = 'real-estate-offline-v14';
 const urlsToCache = [
   'index.html',
   'manifest.json',
@@ -6,16 +6,18 @@ const urlsToCache = [
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://unpkg.com/@babel/standalone/babel.min.js',
   'https://cdn.tailwindcss.com',
-  'https://cdn-icons-png.flaticon.com/512/609/609803.png'
+  'https://cdn-icons-png.flaticon.com/512/3135/3135673.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); }))));
 });
+
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
